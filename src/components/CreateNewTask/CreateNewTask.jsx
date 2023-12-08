5d1126d75e83e8971bccbd314907fb9ce42b26dc
N4rdelli <nardellianelize20@gmail.com>
2023-12-08 10:08:52 -0300

    Projeto finalizado

--- src/components/CreateNewTask/CreateNewTask.jsx
+++ src/components/CreateNewTask/CreateNewTask.jsx
@@ -1,39 +1,51 @@
-import { useState } from 'react';
-import Title from '../Title/Title';
-import styles from './CreateNewTask.module.css';
+import Title from "../Title/Title";
+import styles from "./CreateNewTask.module.css";
+
+import { useContext, useState } from "react";
+import { AppContext } from "../../AppContext";
 
 import addIcon from "../../assets/icon-add.svg";
 
 const CreateNewTask = () => {
-    const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const [error, setError] = useState(false);
+  const {handleTaskAdd} = useContext(AppContext)
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (inputValue.length){
-            setInputValue("")
-        }
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputValue.length) {
+        handleTaskAdd(inputValue);
+      setInputValue("");
+    } else setError(true);
+  };
 
-    const handleChange = (e) => {
-        setInputValue(e.target.value);
-    }
+  const handleChange = (e) => {
+    const target = e.target;
+    if (error) setError(false);
+    setInputValue(target.value);
+  };
 
   return (
     <div className={styles.container}>
-        <Title iconName={"pencil"} text={"Criar nova tarefa"} />
-        <form onSubmit={} className={styles.inputContainer}>
-            <input
+      <Title iconName={"pencil"} text={"Criar nova tarefa"} />
+
+      <form onSubmit={handleSubmit}>
+        <div className={styles.inputContainer}>
+          <input
+            autoFocus
             className={styles.inputText}
             type="text"
-            autoFocus
-            placeholder='Nome da tarefa'
-            />
-            <button type='submit'>
-                <img src={addIcon} alt="Adicionar tarefa" />
-            </button>
-        </form>
+            placeholder="Nome da tarefa"
+            value={inputValue}
+            onChange={handleChange}
+          />
+          <button className={styles.addButton} type="submit">
+            <img src={addIcon} alt="Adicionar tarefa" />
+          </button>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default CreateNewTask
+export default CreateNewTask;
